d43fb981fa46e63b3084a3dac6613d6dd8615094
Zhao Yu Lin <>
2021-01-02 18:59:13 -0500

    added pigLatin(), tested it

--- PigLatin.java
+++ PigLatin.java
@@ -1,21 +1,43 @@
+import java.util.*;
 public class PigLatin{
   public static void main(String[] args){
-    System.out.println(pigLatinSimple("mock"));
-    System.out.println(pigLatinSimple("pie"));
-    System.out.println(pigLatinSimple("david"));
-    System.out.println(pigLatinSimple("aaron"));
+
+    System.out.println(pigLatin("the"));
+    System.out.println(pigLatin("check"));
+    System.out.println(pigLatin("skee"));
+    System.out.println(pigLatin("emu"));
+    System.out.println(pigLatin("grade"));
+
+
   }
 
   public static String pigLatinSimple(String s){
+    String t = s.toLowerCase();
     String[] vowels = new String[]{"a","e","i","o","u"};
 
     for(int i = 0; i < vowels.length; i++){
-      if(s.substring(0,1).equals(vowels[i])){
-        s+="hay";
-        return s;
+      if(t.substring(0,1).equals(vowels[i])){
+        return t + "hay";
       }
     }
 
-    return s.substring(1,s.length()) + s.substring(0,1) + "ay";
+    return t.substring(1,t.length()) + t.substring(0,1) + "ay";
   }
+
+  public static String pigLatin(String s){
+    String t = s.toLowerCase();
+    String[] vowels = new String[]{"a","e","i","o","u"};
+    String[] diagraphs = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    String two = t.substring(0,2);
+
+    for(int i = 0; i < diagraphs.length; i++){
+      if(two.equals(diagraphs[i])){
+        return t.substring(2,t.length()) + two + "ay";
+      }
+    }
+    return pigLatinSimple(t);
+  }
+
+  
+
 }
